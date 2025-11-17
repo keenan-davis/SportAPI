f453b1c1d46667be81a7033265693f5a394aeb58
keenan-davis <st10201316@vcconnect.edu.za>
2025-11-17 15:51:06 +0200

    Docker Implemented

--- SportsTriviaAPI/Program.cs
+++ SportsTriviaAPI/Program.cs
@@ -11,12 +11,10 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 var seed = QuestionsSeed.Questions;
 app.UseHttpsRedirection();
 
--- SportsTriviaAPI/obj/Debug/net8.0/SportsTriviaAPI.AssemblyInfo.cs
+++ SportsTriviaAPI/obj/Debug/net8.0/SportsTriviaAPI.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("6e6bd356-8514-447b-912c-83a40221ce9b")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsTriviaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a75edc35ea0d267dcc3c9452f2294c7a07b66f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsTriviaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsTriviaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
